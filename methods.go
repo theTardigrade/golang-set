bae1a314179a074cfef2009433145f61a20ee0d2
James <jismith1989@googlemail.com>
2019-05-21 22:08:53 +0100

    Create ContainsEvery and ContainsSome methods

--- methods.go
+++ methods.go
@@ -335,16 +335,17 @@ func (d *datum) Remove(values ...interface{}) (success bool) {
 }
 
 // mutex should be read-locked before calling
-func (d *datum) contains(value interface{}) bool {
+func (d *datum) contains(value interface{}) (success bool) {
 	s1 := newStoreDatum(value)
 
 	for _, s2 := range d.store {
 		if d.equalityTest(s1, s2) {
-			return true
+			success = true
+			break
 		}
 	}
 
-	return false
+	return
 }
 
 func (d *datum) Contains(value interface{}) bool {
@@ -352,6 +353,34 @@ func (d *datum) Contains(value interface{}) bool {
 	d.mutex.RLock()
 
 	return d.contains(value)
+}
+
+func (d *datum) ContainsEvery(values ...interface{}) (success bool) {
+	defer d.mutex.RUnlock()
+	d.mutex.RLock()
+
+	for _, v := range values {
+		if !d.contains(v) {
+			return
+		}
+	}
+
+	success = true
+	return
+}
+
+func (d *datum) ContainsSome(values ...interface{}) (success bool) {
+	defer d.mutex.RUnlock()
+	d.mutex.RLock()
+
+	for _, v := range values {
+		if d.contains(v) {
+			success = true
+			break
+		}
+	}
+
+	return
 }
 
 func (d *datum) Pop() (value interface{}) {
